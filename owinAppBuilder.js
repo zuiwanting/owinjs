0941ae6074cfdce0bdae615d403f86625e55f948
domabo <domabo@domabo.com>
2014-03-10 21:41:01 -0500

    owinAppBuilder

--- owinAppBuilder.js
+++ owinAppBuilder.js
@@ -25,7 +25,7 @@ app.use = function(mw){
  };
 
 app.build = function(){
-    var mw = this.properties[constants.builder.DefaultMiddleware].concat(this.middleware).concat(this.properties[constants.builder.DefaultMiddleware]);
+    var mw = this.properties[constants.builder.DefaultMiddleware].concat(this.middleware).concat(this.properties[constants.builder.DefaultApp]);
     var fn = compose(mw);
     var self = this;
     
@@ -74,9 +74,11 @@ function owinRespondMiddleware(next){
     this.response.setHeader('X-Powered-By', 'OWIN-JS');
     
     return next().then(
-                       function (){ return Promise.from(null);
+                       function (){
+                       return Promise.from(null);
                        },
-                       function (err){   owinDefaultError.call(owin,err);
+                       function (err){
+                       owinDefaultError.call(owin,err);
                        return Promise.from(null);  }
                        );
 }
@@ -84,16 +86,18 @@ function owinRespondMiddleware(next){
 function owinDefaultApp(next){
     if (this[constants.owinjs.Error])
         return Promise.reject(this[constants.owinjs.Error]);
-    else if (this.response.statusCode === null)
+    else if (this.response.statusCode == null)
+    {
         return Promise.reject(404);
+    }
     else
     {
-        return Promise.from(null);
+         return Promise.from(null);
     }
 }
 
 function owinDefaultError(err){
-    console.log("OwinJS Error Occured in Pipeline " + this.request.scheme + ":\\"+ this.request.path + "\r" + err);
+    console.log("Server Error " + err + " for " + this.request.path);
     
     if (err==404)
     {
