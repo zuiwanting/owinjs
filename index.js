f063658f40576189c2e3d554b65800332ecc6f34
domabo <domabo@domabo.com>
2014-03-23 11:19:45 -0500

    Release with appFunc not nodeFunc, and use owinjs.createServer

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ exports.toHttp = require('./lib/owinHttp.js');
 exports.context = require('./lib/owinContext.js');
 exports.middleware = require('./lib/owinMiddleware.js');
 exports.constants = require('./lib/owinConstants.js');
+exports.createServer = require('./lib/owinCreateServer.js').createServer;
